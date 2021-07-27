6c10f04a32571260943bd40300542b8ec9e4a420
Brandon Morgan <59944082+OUStudent@users.noreply.github.com>
2021-07-27 15:56:21 -0500

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='gym_tic_tac_toe3D',
+      version='0.0.1',
+      description='Gym Environment for 3D Tic Tac Toe',
+      author='Brandon Morgan',
+      author_email="morganscottbrandon@gmail.com",
+      license="MIT",
+      install_requires=['gym',
+                        'matplotlib',
+                        'numpy']
+      )
